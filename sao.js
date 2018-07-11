649d333c986f8cddf836cb77a59ffe0436205eb9
elcarim5efil <elcarim5efil@gmail.com>
2018-07-11 14:44:51 +0800

    update

--- sao.js
+++ sao.js
@@ -1,5 +1,11 @@
 const _ = require('./template/scripts/component-template/helper');
 
+const registry = {
+  npmjs: 'https://registry.npmjs.org',
+  taobao: 'https://registry.npm.taobao.org',
+  netease: 'http://rnpm.hz.netease.com',
+};
+
 module.exports = {
   prompts: {
     scope: {
@@ -10,6 +16,14 @@ module.exports = {
       message: `请输入组件前缀:`,
       defaults: 'kl'
     },
+    npmRegistryName: {
+      type: 'rawlist',
+      message: `请选择 npm 源`,
+      choices: Object.keys(registry)
+    }
+  },
+  data(answers) {
+    answers.npmRegistry = registry[answers.npmRegistryName];
   },
   skipInterpolation: [
     'scripts/**/*'
@@ -19,5 +33,6 @@ module.exports = {
     return Object.assign(folders, {
       'gitignore': '.gitignore'
     });
-  }
+  },
+  // installDependencies: true,
 }
